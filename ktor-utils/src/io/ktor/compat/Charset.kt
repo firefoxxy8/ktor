ab0ba7ad606c1d745bad9db6a85f0d3be9f00eff
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-06-28 14:04:11 +0300

    Add base64 mpp encoding/decoding

--- ktor-utils/src/io/ktor/compat/Charset.kt
+++ ktor-utils/src/io/ktor/compat/Charset.kt
@@ -1,11 +1,6 @@
 package io.ktor.compat
 
-import kotlinx.io.charsets.*
-
 expect fun Char.isLowerCase(): Boolean
 
-expect fun String.toCharArray(): CharArray
+fun String.toCharArray(): CharArray = CharArray(length) { get(it) }
 
-expect fun encodeBase64(string: String, charset: Charset = Charsets.UTF_8): String
-
-expect fun decodeBase64(string: String, charset: Charset = Charsets.UTF_8): String
